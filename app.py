3f376aefd2006a22112d5a38b07fd1e40e4697fb
frieda <frieda.porfert@posteo.de>
2023-03-14 12:14:11 +0100

    now I fixed the articles and deactivated debug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
@@ -8,6 +8,14 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return redirect("article?article=about me")
+
+@app.route("/article")
+def article():
+    return render_template("article.html")
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
